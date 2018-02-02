b7064d9c69e6b666485090f291acc7ccd4a74b95
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-02-02 23:38:35 +0100

    Prova

--- AnalisiDC.php
+++ AnalisiDC.php
@@ -1,9 +1,9 @@
 <?php
     include_once(__DIR__."/Dc/Dc.php");
     
+    // Ciao
     use Dc\Dc  ;
-    
+
     $test = new Dc(__DIR__."/Dati/3671_20171210_171210_DC.TXT");
     $test->mostraInformazioni();
 
-?>
