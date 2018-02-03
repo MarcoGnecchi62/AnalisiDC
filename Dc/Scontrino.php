e0c29cdaa585f47f1c99c2bd46b4ccd6ff510f4c
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-02-03 09:28:12 +0100

    Sistemazione Caricamento Vendite
    Aggiunta la logica per il caricamento delle righe vendita (tipo S). Indicazioni prese dal manuale Gamba Bruno/NCR

--- Dc/Scontrino.php
+++ Dc/Scontrino.php
@@ -21,15 +21,32 @@
         private function caricaVendite() {
             $righe = array();
             foreach ($this->righe as $riga) {
-                if (preg_match('/^.{31}:S:(\d)(\d)(\d):(\d{4}):.{3}(.{13})((?:\+|\-)\d{4})(\d|\.)(\d{3})(.)(\d{9})$/', $riga, $matches)) {
+                if (preg_match('/^.{31}:S:(\d)(\d)(\d):(\d{4}):.{3}(.{13})((?:\+|\-)\d{4})(\d|\.)(\d{3})(\+|\-|\*)(\d{9})$/', $riga, $matches)) {
                     array_push($righe, $riga);
                     
-                    // verifica se decimale
-                    $barcode = trim($matches[5]);
-                    $quantita = 1;
-                    $importo = 1;
+                    $codice1 = $matches[1];
+                    $codice3 = $matches[2];
+                    $codice3 = $matches[3];
+                    $reparto = $matches[4];
+                    $plu = trim($matches[5]);
+                    if ('.' == $matches[7]) {
+                        $quantita = ($matches[6].'.'.$matches[8])*1;
+                        $unitaImballo = 0.0;
+                    } else {
+                        $quantita = $matches[6]*1;
+                        $unitaImballo = $matches[8]/10;
+                    }
+                    $importoUnitario = 1;
+                    $importoTotale = 1;
+                    if ('*' == $matches[9]) {
+                        $importoUnitario = $matches[10]*1;
+                        $importoTotale = $quantita*$importoUnitario;
+                    } else {
+                        $importoUnitario = ($matches[9].$matches[10])*1;
+                        $importoTotale = $importoUnitario;
+                    }
                     
-                    array_push($this->vendite, new Vendita($barcode, $quantita, $importo));
+                    array_push($this->vendite, new Vendita($codice1, $codice2, $codice3, $reparto, $plu, $quantita, $unitaImballo, $importoUnitario, $importoTotale));
                 } 
             }
         }
--- Dc/Vendita.php
+++ Dc/Vendita.php
@@ -1,15 +1,32 @@
 <?php
     namespace Dc;
-    
+	
+	/*
+	 *RECORD 'S'
+	 *code1 = 0 Money trasnfer, 1 Sale trans., 2 Receipt.abort, 3 Training, 4 Re-entry, 5 Stock count, 6 Transfers, 7 Layaways, 8 Suspended, 9 Reset mode
+	 *code2 = 0 Normal data entry,1 Negative subdept. or PLU, 1 Negative subdept. or PLU (bottle refund),4 Item return, 5 Trans. return, 6 Trans. void, 7 Item void, 8 Error Correct
+	 *code3 = 0 Manual entry, 1 Scanner entry, 8 Manual entry (special sale), 9 Scanner entry (special sale)
+    */
     class Vendita {
-        private $barcode = '';
+		private $codice1 = 1;
+		private $codice2 = 0;
+		private $codice3 = 1;
+		private $reparto = '0000';
+        private $plu = '';
         private $quantita = 0.0;
-        private $importo = 0.0;
-        
-        function __construct($barcode, $quantita, $importo) {
-            $this->barcode = $barcode;
+		private $unitaImballo = 0;
+        private $importoUnitario = 0.0;
+		private $importoTotale = 0.0;
+		
+        function __construct($codice1, $codice2, $codice3, $reparto, $plu, $quantita, $unitaImballo, $importoUnitario, $importoTotale) {
+            $this->codice1 = $codice1;
+			$this->codice2 = $codice2;
+			$this->codice3 = $codice3;
+			$this->plu = $plu;
             $this->quantita = $quantita;
-            $this->importo = $importo;
+            $this->unitaImballo = $unitaImballo;
+			$this->importoUnitario = $importoUnitario;
+			$this->importoTotale = $importoTotale;
         }
         
         function __destruct() {}
